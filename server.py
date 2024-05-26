80533e5b2769b421b00e30bf989b449e6168874d
degert <egde@users.noreply.github.com>
2024-05-26 13:07:13 +0200

    load static

--- server.py
+++ server.py
@@ -8,14 +8,14 @@ import os
 app = FastAPI()
 
 static_dir = "static"
-app.mount("/", StaticFiles(directory=static_dir), name="static")
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 # Serve the index.html file when accessing the root URL
 @app.get("/")
 async def read_root():
     index_path = os.path.join(static_dir, "index.html")
     return FileResponse(index_path)
-    
+
 @app.get("/api")
 def read_root():
     return {"Hello": "World"}
